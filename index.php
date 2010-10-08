e5770126781a3285c2c473a8fabc7a1e1a038159
Keisuke SATO <riaf@nequal.jp>
2010-10-09 02:52:08 +0900

    携帯対応

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php require dirname(__FILE__)."/__settings__.php"; app(); ?>
 <app>
 	<handler>
+        <module class="jp.riaf.flow.module.MobileFlowModule" />
+        <module class="jp.riaf.flow.module.MobileTemplateModule" />
         <maps class="Itsukuru">
             <map name="index" url="" template="index.html" />
             <map name="bookmark" template="bookmark.html" />
